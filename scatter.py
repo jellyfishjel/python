f2ccea1e5ef1daa32064f1b7a4509825e80d6519
jellyfishjel <meimeicute06@gmail.com>
2025-06-01 12:54:39 +0700

    Update scatter.py

--- scatter.py
+++ scatter.py
@@ -51,6 +51,6 @@ fig.data[0].marker.color = '#00BFFF'  # màu DeepSkyBlue (xanh dương sáng)
 
 # Tăng chiều cao biểu đồ
 fig.update_layout(
-    height=600,
+    height= 700,
 )
 st.plotly_chart(fig, use_container_width=True)
