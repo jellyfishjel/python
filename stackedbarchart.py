9a5e8554a6f722956e3de0f1eb34d37562562946
jellyfishjel <meimeicute06@gmail.com>
2025-06-12 20:55:18 +0700

    Update stackedbarchart.py

--- stackedbarchart.py
+++ stackedbarchart.py
@@ -100,7 +100,8 @@ fig_line = px.line(
 )
 
 fig_line.update_traces(
-    hovertemplate='Average Job Offers=%{y:.2f}<extra></extra>'
+    line=dict(width=2),
+    marker=dict(size=6)
 )
 fig_line.update_layout(
     margin=dict(t=40, l=40, r=40, b=40),
